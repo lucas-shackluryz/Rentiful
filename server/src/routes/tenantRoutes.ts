dcae376758b5d282a7101ad54302d19d8e3e1c0c
lucas-shackluryz <lucas.shackluryz@gmail.com>
2025-07-04 19:12:38 -0300

    [ ADD ] Create Property Controller and Routes

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { getTenant, createTenant, updateTenant } from "../controllers/tenantControllers";
+import { getTenant, createTenant, updateTenant, getCurrentResidences } from "../controllers/tenantControllers";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getTenant);
 router.put("/:cognitoId", updateTenant);
 router.post("/", createTenant);
+router.get("/:cognitoId/current-residences", getCurrentResidences);
 
 export default router;
