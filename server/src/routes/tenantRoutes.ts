5a4c86af926df5c4ccd71e4dda4cfd537a669ef2
lucas-shackluryz <lucas.shackluryz@gmail.com>
2025-07-05 10:49:18 -0300

    [ ADD ] Create Lease Controlles and Routes

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getTenant, createTenant, updateTenant, getCurrentResidences } from "../controllers/tenantControllers";
+import { getTenant, createTenant, updateTenant, getCurrentResidences, removeFavoriteProperty, addFavoriteProperty } from "../controllers/tenantControllers";
 
 const router = express.Router();
 
@@ -7,5 +7,7 @@ router.get("/:cognitoId", getTenant);
 router.put("/:cognitoId", updateTenant);
 router.post("/", createTenant);
 router.get("/:cognitoId/current-residences", getCurrentResidences);
+router.post("/:cognitoId/favorites/:propertyId", addFavoriteProperty);
+router.delete("/:cognitoId/favorites/:propertyId", removeFavoriteProperty);
 
 export default router;
